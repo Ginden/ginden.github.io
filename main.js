442a4ca27094fc96c8468c5039d021a43a97ff39
Michał Wadas <michalwadas@gmail.com>
2014-11-07 21:24:59 +0100

    JS

---
+++ main.js
@@ -0,0 +1,29 @@
+$('.tiles.activable > div').click(function() {
+    var had = $(this).hasClass('active');
+    $(this).parent().children().removeClass('active');
+    if (!had)
+        $(this).addClass('active');
+    onResize();
+});
+
+var calculationFuncs = {
+    npm: function($el) {
+        $el = $($el);
+        $el.height($el.width() * 0.39);
+    },
+    square: function($el) {
+        $el = $($el);
+        $el.height($el.width());
+    },
+    php: function($el) {
+        $el = $($el);
+        $el.height($el.width()*(300/159));
+    }
+}
+function onResize(){
+    $('*[data-calculated-size]').each(function(index,el){
+        calculationFuncs[$(el).attr('data-calculated-size')](el);
+    });
+}
+$(window).resize(onResize);
+onResize();
